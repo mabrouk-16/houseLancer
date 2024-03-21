c66353af58cc871b8bde69a37eb654a8dc4008c3
Mohamed Masoud <116065990+MMohamed1998@users.noreply.github.com>
2024-03-21 09:56:31 +0200

    new commit

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,7 +7,6 @@ import AddProject from '../AddProject/AddProject';
 
 function Projects() {
   const nav = useNavigate();
-  console.log("last")
   const { data, isFetching ,isSuccess} = useGetProjectsQuery();
   const [projects, setProjects] = useState([]);
 
@@ -36,6 +35,7 @@ function Projects() {
   return (
     <>
     
+      <div className={`${styles.font}`}>
       {isFetching ? (
         <div id="loading-wrapper">
           <div id="loading-text">LOADING</div>
@@ -59,23 +59,11 @@ function Projects() {
         </div>
       )}:<div>
   <>
-<div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className={`modal-dialog modal-L ${styles.add_Project}`}>
-    <div className={`modal-content ${styles.add_Project}`}>
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLabel">Add Project</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-       <AddProject/>
-      </div>
-      
-    </div>
-  </div>
-</div>
+   <AddProject/>  
   </>
 
     </div> 
+      </div>
     </>
   );
 }
